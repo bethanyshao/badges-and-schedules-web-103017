b287bd9e39e07e2bd151f26a7de42023c6e888b5
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 12:50:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-
+def batch_badge_creator(array)
+  badges = []
+  array.each do |name|
+    badges.push(badge_maker(name))
+  end
+  badges
 end
 
 def assign_rooms
